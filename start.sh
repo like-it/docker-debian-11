fa0554f549b7e6edd4db2c691837d1f653b0ecb2
Remco van der Velde <remco.pc@outlook.com>
2022-11-09 22:49:56 +0100

    update

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 echo "composer install -n" >> ~/.bashrc
+echo "mkdir -p /Application/Bin" >> ~/.bashrc
 echo "cp /Application/vendor/r3m/framework/Bin/r3m.php /Application/Bin/R3m.php" >> ~/.bashrc
 echo "php /Application/Bin/R3m.php bin r3m.io" >> ~/.bashrc
 echo "r3m.io configure environment development" >> ~/.bashrc
